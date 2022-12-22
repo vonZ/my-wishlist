094ccae078d3e780a211cdf3d43a5ff57270062d
vonZ <vv.zeipel@gmail.com>
2022-12-22 18:49:53 +0100

    update trpc

--- src/utils/trpc.tsx
+++ src/utils/trpc.tsx
@@ -5,14 +5,17 @@ import superjson from "superjson";
 
 import { type AppRouter } from "../server/trpc/router/_app";
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+// const getBaseUrl = () => {
+//   if (typeof window !== "undefined") return ""; // browser should use relative url
+//   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+//   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+// };
 
 export const trpc = createTRPCNext<AppRouter>({
   config() {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
     return {
       transformer: superjson,
       links: [
@@ -22,12 +25,27 @@ export const trpc = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url,
         }),
       ],
     };
   },
-  ssr: false,
+  ssr: true,
+  responseMeta({ clientErrors }) {
+    if (clientErrors.length) {
+      // propagate http first error from API calls
+      return {
+        status: clientErrors[0]?.data?.httpStatus ?? 500,
+      };
+    }
+    // cache request for 1 day + revalidate once every second
+    const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+    return {
+      headers: {
+        "cache-control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+      },
+    };
+  },
 });
 
 /**
