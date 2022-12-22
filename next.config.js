d58578f4521c309fe6d349078fb34e21e671f93b
vonZ <vv.zeipel@gmail.com>
2022-12-22 22:31:14 +0100

    test new next config

--- next.config.js
+++ next.config.js
@@ -3,16 +3,20 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+// !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
-/** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+// /** @type {import("next").NextConfig} */
+// const config = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   i18n: {
+//     locales: ["en"],
+//     defaultLocale: "en",
+//   },
+// };
+// export default config;
+
+module.exports = {
   async headers() {
     return [
       {
@@ -35,4 +39,3 @@ const config = {
     ];
   },
 };
-export default config;
