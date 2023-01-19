ad0c05305b19ae85ab2a9d608c68305d872891f5
vonZ <vv.zeipel@gmail.com>
2023-01-19 22:11:20 +0100

    added stories to steps

---
+++ .storybook/preview.js
@@ -0,0 +1,3 @@
+import "../src/styles/globals.css";
+
+export const decorators = [(Story) => <Story />];
---
+++ src/components/step/steps.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryFn } from "@storybook/react";
+
+import { ListDate as ListDateComponent } from "./listDate";
+import { ListSummary as ListSummaryComponent } from "./listSummary";
+import { ListTitle as ListTitleComponent } from "./listTitle";
+
+export default {
+  title: "Example/Steps",
+} as Meta;
+
+const ListTitleStory: StoryFn<typeof ListTitleComponent> = (args) => (
+  <ListTitleComponent {...args} />
+);
+
+export const ListTitle = ListTitleStory.bind({});
+ListTitle.args = {
+  onSubmit: () => null,
+};
+
+const ListDateStory: StoryFn<typeof ListDateComponent> = (args) => (
+  <ListDateComponent {...args} />
+);
+
+export const ListDate = ListDateStory.bind({});
+ListDate.args = {
+  onSubmit: () => null,
+};
+
+const ListSummaryStory: StoryFn<typeof ListSummaryComponent> = (args) => (
+  <ListSummaryComponent {...args} />
+);
+
+export const ListSummary = ListSummaryStory.bind({});
+ListSummary.args = {
+  onSubmit: () => null,
+};
